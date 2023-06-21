b735162e5dd3ddd72b88c47e619906c5858a3da2
Maduhansi Lakshini <maduhansilakshini@gmail.com>
2023-06-21 09:11:46 +0900

    netflix-clone

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "cd9e04987728c3239547d9fcaec83232";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+// https://www.themoviedb.org/discover/tv?api_key=cd9e04987728c3239547d9fcaec83232&with_networks=213
+
+export default requests;
